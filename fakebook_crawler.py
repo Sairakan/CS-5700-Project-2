c777f079445c43cd87d98f64f6bf7255a72a746d
Jae Son <sjson421@gmail.com>
2018-10-10 15:27:50 -0400

    spider example added

--- fakebook_crawler.py
+++ fakebook_crawler.py
@@ -101,7 +101,42 @@ Content-Length: ''' + str(40 + len(username) + len(password) + len(csrftoken)) +
 # takes the socket, the csrftoken, and the sessionid of the logged-in user and 
 # crawls Fakebook to find the secret flags
 def crawl(s, csrftoken, sessionid):
+
     pass
+
+##### And finally here is our spider. It takes in an URL, a word to find,
+##### and the number of pages to search through before giving up
+####def spider(url, word, maxPages):  
+####    pagesToVisit = [url]
+####    numberVisited = 0
+####    foundWord = False
+####    # The main loop. Create a LinkParser and get all the links on the page.
+####    # Also search the page for the word or string
+####    # In our getLinks function we return the web page
+####    # (this is useful for searching for the word)
+####    # and we return a set of links from that web page
+####    # (this is useful for where to go next)
+####    while numberVisited < maxPages and pagesToVisit != [] and not foundWord:
+####        numberVisited = numberVisited +1
+####        # Start from the beginning of our collection of pages to visit:
+####        url = pagesToVisit[0]
+####        pagesToVisit = pagesToVisit[1:]
+####        try:
+####            print(numberVisited, "Visiting:", url)
+####            parser = LinkParser()
+####            data, links = parser.getLinks(url)
+####            if data.find(word)>-1:
+####                foundWord = True
+####                # Add the pages that we visited to the end of our collection
+####                # of pages to visit:
+####                pagesToVisit = pagesToVisit + links
+####                print(" **Success!**")
+####        except:
+####            print(" **Failed!**")
+####    if foundWord:
+####        print("The word", word, "was found at", url)
+####    else:
+####        print("Word never found")
 
 class LinkParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
