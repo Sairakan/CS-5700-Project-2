783a0e0b0778eaf70c79b64e614ced6b7eea84c9
jteng <akariass@gmail.com>
2018-10-12 14:34:05 -0400

    progress on crawler, need to fix chunked encoding parsing

--- fakebook_crawler.py
+++ fakebook_crawler.py
@@ -23,9 +23,6 @@ DEFAULT_PORT = 80
 username = args.username
 password = args.password
 
-csrftoken = ''
-sessionid = ''
-
 # takes a HTTP message and returns the raw header and html as separate strings
 def parseResponse(response):
     s = response.split('\r\n\r\n', 1)
@@ -51,7 +48,9 @@ def parseHeaders(rawheaders):
             headers[header[0]] = header[1]
     return headers
 
-def getResponse(s, message):
+def getResponse(message, csrftoken, sessionid):
+    global s
+    print 'MESSAGE:\n' + message
     s.send(message)
     response = ''
     try:
@@ -66,6 +65,10 @@ def getResponse(s, message):
     rawheaders, rawhtml = parseResponse(response)
     headers = parseHeaders(rawheaders)
     responsecode = rawheaders.splitlines()[0].split()[1]
+    # check for connection close
+    if headers.has_key('Connection') and headers['Connection'] == 'close':
+        s.close()
+        s = socket.create_connection((base_url, DEFAULT_PORT))
     # check redirect
     if responsecode == '301':
         newloc = headers['Location']
@@ -79,94 +82,83 @@ Cookie: csrftoken=''' + csrftoken + '''; sessionid=''' + sessionid
     # check server error
     if responsecode == '500':
         s.close()
-        s.connect((base_url, DEFAULT_PORT))
-        return getResponse(s, message)
+        s = socket.create_connection((base_url, DEFAULT_PORT))
+        return getResponse(message, csrftoken, sessionid)
+    # read rest of message body
     if headers.has_key('Content-Length'):
         bodylength = int(headers['Content-Length'])-1
         while len(rawhtml) < bodylength:
             rawhtml += s.recv(8192)
-    elif headers.has_key('Transfer-Encoding:') and headers['Transfer-Encoding'].lower() == 'chunked':
-        chunksize = rawhtml.splitlines()[0]
-        rawhtml = rawhtml[len(chunksize):]
-        while int(chunksize) > 0:
-            nexthtml = s.recv(8192)
-            chunksize = nexthtml.splitlines()[0]
-            rawhtml += nexthtml[len(chunksize):]
+    # read chunked encoding
+    elif headers.has_key('Transfer-Encoding') and headers['Transfer-Encoding'].lower() == 'chunked':
+        chunks = rawhtml.split('\r\n')[1::2]
+        print chunks
+        rawhtml = ''.join(chunks)
 
+    print 'RESPONSE: (' + str(len(response)) + ')\n' + rawheaders + '\n'
+    print rawhtml + '\n'
     return headers, rawhtml
 
 # Performs a GET from the fakebook homepage to get a CSRF token
-def getCSRFToken(s):
+def getCSRFToken():
+    global s
     message = 'GET http://' + base_url + '''/accounts/login/?next=/fakebook/ HTTP/1.1
 Host: cs5700f18.ccs.neu.edu\r\n\r\n'''
-    headers, rawhtml = getResponse(s, message)
+    headers, rawhtml = getResponse(message, '', '')
     return getCookie(headers, 'csrftoken')
 
 
 # logs into Fakebook using the given socket, username, and password, and returns
 # the sessionid for the user session.
-def login(s, username, password):
+def login(username, password, csrftoken):
+    global s
     headers = 'POST http://' + base_url + '''/accounts/login/ HTTP/1.1
 Host: cs5700f18.ccs.neu.edu
 Cookie: csrftoken=''' + csrftoken + ''';
 Content-Type: application/x-www-form-urlencoded
-Content-Length: ''' + str(40 + len(username) + len(password) + len(csrftoken)) + '\r\n\r\n'
+Content-Length: ''' + str(60 + len(username) + len(password) + len(csrftoken)) + '\r\n\r\n'
     content = 'username=' + username + '&password=' + password \
-        + '&csrfmiddlewaretoken=' + csrftoken + '&next=%2Ffakebook%2F' + '\r\n'
+        + '&csrfmiddlewaretoken=' + csrftoken + '&next=%2Ffakebook%2F\r\n'
     message = headers + content
-    headers, rawhtml = getResponse(s, message)
+    headers, rawhtml = getResponse(message, csrftoken, '')
     return getCookie(headers, 'sessionid')
 
 # takes the socket, the csrftoken, and the sessionid of the logged-in user and 
 # crawls Fakebook to find the secret flags
-def crawl(s, csrftoken, sessionid):
+def crawl(csrftoken, sessionid):
+    global s
     secretFlagList = []
     secretFlagTag = "<h2 class='secret_flag' style=\"color:red\">FLAG: "
-    pagesToVisit = [base_url]
+    pagesToVisit = ['/fakebook/']
+    visited = []
 
-    i = 0
-    while i < range(len(pagesToVisit)):
-        url = pagesToVisit[i]
-        if len(secretFlagList) == 5:
-            break
-        try:
-            print("Visiting: " + url)
-            
-            # TODO
-            message = 'GET ' + url + ''' HTTP/1.1
+    while len(secretFlagList) < 5:
+        url = pagesToVisit[0]
+        print("Visiting: " + url)
+        visited.append(url)
+        pagesToVisit.remove(url)
+        
+        # TODO
+        message = 'GET ' + url + ''' HTTP/1.1
 Host: cs5700f18.ccs.neu.edu
-Cookie: csrftoken=''' + csrftoken + '''; sessionid=''' + sessionid
+Cookie: csrftoken=''' + csrftoken + '; sessionid=' + sessionid + '\r\n\r\n'
 
-            print ('Message:\n' + message)
-            s.send(message)
-            rawhtml = ''
-            try:
-                readable, writable, errored = select.select([s,], [], [], 30)
-            except select.error:
-                s.close()
-                exit()
-            if len(readable) > 0:
-                rawhtml = s.recv(8192)
-            else:
-                print('timeout')
+        headers, rawhtml = getResponse(message, csrftoken, sessionid)
+        # search for and add secret flag if it exists
+        if secretFlagTag in rawhtml:
+            secretFlagIndex = rawhtml.find(secretFlagTag)+len(secretFlagTag)
+            secretFlag = rawhtml[secretFlagIndex:secretFlagIndex + 64]
+            secretFlagList.append(secretFlag)
+        else:
+            print ("flag not here yo \n\n")
 
-            print ("\nResponse: ")
-            print (rawhtml)
-            if secretFlagTag in rawhtml:
-                secretFlagIndex = rawhtml.find(secretFlagTag)+len(secretFlagTag)
-                secretFlag = rawhtml[secretFlagIndex:secretFlagIndex + 64]
-                secretFlagList.append(secretFlag)
-            else:
-                print ("flag not here yo \n\n")
-
-            # Adds links in current page to list of links to crawl through
-            parser = LinkParser()
-            linkList = parser.getLinks(rawhtml, url)
-            pagesToVisit.extend(linkList)
-            print (pagesToVisit)
-        except:
-            print("**Failed!**")
-        i += 1
+        # Adds links in current page to list of links to crawl through
+        parser = LinkParser()
+        linkList = parser.getLinks(rawhtml, url)
+        for link in linkList:
+            if visited.count(link) == 0:
+                pagesToVisit.append(link)
+        #print (pagesToVisit)
 
     for flag in secretFlagList:
         print(flag)
@@ -178,7 +170,7 @@ class LinkParser(HTMLParser):
             for name, value in attrs:
                 if name == "href":
                     if value[0] == '/':
-                        newUrl = self.baseUrl + value
+                        newUrl = value
                         if newUrl not in self.list:
                             self.list = self.list + [newUrl]
     
@@ -190,15 +182,13 @@ class LinkParser(HTMLParser):
         return self.list
 
 ################################################################################
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((base_url, DEFAULT_PORT))
+s = socket.create_connection((base_url, DEFAULT_PORT))
 
 # GET from login page to get csrf token
-csrftoken = getCSRFToken(s)
+csrftoken = getCSRFToken()
 
 # login and get sessionid
-sessionid = login(s, username, password)
-print(sessionid)
+sessionid = login(username, password, csrftoken)
 
 # Event loop
-crawl(s, csrftoken, sessionid)
+crawl(csrftoken, sessionid)
